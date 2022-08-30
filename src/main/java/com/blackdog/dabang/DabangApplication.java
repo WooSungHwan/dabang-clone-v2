0164fb2f912e8edbf74b8ee852800bf3a60c188e
WooSungHwan <doqndnffo@gmail.com>
2022-08-30 16:54:18 +0900

    Initial commit

---
+++ src/main/java/com/blackdog/dabang/DabangApplication.java
@@ -0,0 +1,13 @@
+package com.blackdog.dabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DabangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackdog/dabang/DabangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackdog.dabang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
